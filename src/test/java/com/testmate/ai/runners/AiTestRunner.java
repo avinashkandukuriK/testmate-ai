95733e305cfa9c8eb9e7ca8b8e87ec59370f24cb
avinash kandukuri <90058702+avinashkandukuriK@users.noreply.github.com>
2026-05-22 22:55:21 -0500

    Add TestNG Cucumber runner

---
+++ src/test/java/com/testmate/ai/runners/AiTestRunner.java
@@ -0,0 +1,17 @@
+package com.testmate.ai.runners;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+        features = "src/test/resources/features",
+        glue = "com.testmate.ai.stepdefs",
+        plugin = {
+                "pretty",
+                "html:target/cucumber-report.html",
+                "json:target/cucumber-report.json"
+        },
+        monochrome = true
+)
+public class AiTestRunner extends AbstractTestNGCucumberTests {
+}
